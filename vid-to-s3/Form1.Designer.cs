a73ab8e736f9c95df6f4a9dba1a8af6b32fbe119
skylersaucedo <skyler.saucedo@gmail.com>
2024-02-03 10:07:55 -0600

    Add project files.

---
+++ vid-to-s3/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace vid_to_s3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSelectVideo = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnSendVideo = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSelectVideo
+            // 
+            this.btnSelectVideo.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSelectVideo.Location = new System.Drawing.Point(504, 12);
+            this.btnSelectVideo.Name = "btnSelectVideo";
+            this.btnSelectVideo.Size = new System.Drawing.Size(173, 31);
+            this.btnSelectVideo.TabIndex = 0;
+            this.btnSelectVideo.Text = "select video";
+            this.btnSelectVideo.UseVisualStyleBackColor = true;
+            this.btnSelectVideo.Click += new System.EventHandler(this.btnSendVideo_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(486, 31);
+            this.textBox1.TabIndex = 1;
+            // 
+            // btnSendVideo
+            // 
+            this.btnSendVideo.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSendVideo.Location = new System.Drawing.Point(12, 49);
+            this.btnSendVideo.Name = "btnSendVideo";
+            this.btnSendVideo.Size = new System.Drawing.Size(665, 73);
+            this.btnSendVideo.TabIndex = 2;
+            this.btnSendVideo.Text = "send video";
+            this.btnSendVideo.UseVisualStyleBackColor = true;
+            this.btnSendVideo.Click += new System.EventHandler(this.btnSendVideo_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(698, 135);
+            this.Controls.Add(this.btnSendVideo);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnSelectVideo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        public System.Windows.Forms.Button btnSelectVideo;
+        public System.Windows.Forms.TextBox textBox1;
+        public System.Windows.Forms.Button btnSendVideo;
+    }
+}
+
---
+++ vid-to-s3/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Amazon;
+using Amazon.Extensions.NETCore.Setup;
+using Amazon.Runtime.CredentialManagement;
+using Amazon.Runtime;
+using Amazon.S3;
+using Amazon.S3.Transfer;
+using System.Xml.Linq;
+
+//
+// AWS credentials done in AWS CLI. Use CMD and run 'aws configure' to set. 
+//
+namespace vid_to_s3
+{
+    public partial class Form1 : Form
+    {
+        AWSCredentials awsCredentials;
+        public string bucketName = "tsi-raw-videos";
+        public string subDirectoryInBucket = "test-subdir"; // Optional subdirectory within the bucket
+        public string fileNameInS3 = "testvideo-feb2-2.avi"; // Desired file name in S3
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            var chain = new CredentialProfileStoreChain();
+
+            if (chain.TryGetAWSCredentials("videouploader", out awsCredentials))
+            {
+                MessageBox.Show("Credentials profile found...");
+                MessageBox.Show(awsCredentials.ToString());
+            }
+        }
+
+        string selectedFileName { get; set; }
+
+        private void btnSendVideo_Click(object sender, EventArgs e)
+        {
+
+            using (OpenFileDialog ofd = new OpenFileDialog())
+            {
+                ofd.InitialDirectory = "C:\\";
+                ofd.Filter = "Movie files (*.avi) | *.avi";
+                ofd.FilterIndex = 0;
+                ofd.RestoreDirectory = true;
+
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    selectedFileName = ofd.FileName;
+                    textBox1.Text = selectedFileName;
+                }
+
+                MessageBox.Show("you selected: " + selectedFileName);
+            }
+           
+        }
+
+        private void btnSendVideo_Click_1(object sender, EventArgs e)
+        {
+
+            //send video to S3
+
+            if (selectedFileName != "")
+            {
+                MessageBox.Show("you selected: " + selectedFileName);
+                MessageBox.Show("sending it!");
+                _ = sendVidFile(selectedFileName, bucketName, subDirectoryInBucket, fileNameInS3);
+                
+            }
+        }
+
+        public static async Task sendVidFile(string fname, string bucketName, string subDirectoryInBucket, string fileNameInS3)
+        {
+            
+            var client = new AmazonS3Client(RegionEndpoint.USEast1); // Choose your desired region
+
+            var transferUtility = new TransferUtility(client);
+
+            await transferUtility.UploadAsync(fname, bucketName, $"{subDirectoryInBucket}/{fileNameInS3}");
+
+            Console.WriteLine("File uploaded successfully!");
+        }
+    }
+}
